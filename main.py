ab789ed3d012972f4d98c904a97029377da02139
Orion <orion@gmail.com>
2025-11-03 14:13:52 -0400

    branch feature/x

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Instruções:
 
 # Função para calcular a média
 def calcular_media(lista):
-    # TODO: implementar a soma dos wewewweweelementos e dividir pelo tamanho da lista
+    # TODO: implementar awwwewwe soma dos wewewweweelementos e dividir pelo tamanho da lista
     pass
 
 
