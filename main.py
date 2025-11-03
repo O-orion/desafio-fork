f9b38a468de444ee8ef864fc2733fc289315491a
ArturVinciuss <vinimota@hotmail.com.br>
2025-11-03 20:35:31 -0300

    funções completadas

--- main.py
+++ main.py
@@ -17,21 +17,44 @@ Instruções:
 # Função para calcular a média
 def calcular_media(lista):
     # TODO: implementar a soma dos elementos e dividir pelo tamanho da lista
-    pass
-
+    return sum(lista) / len(lista) 
 
 # Função para calcular a mediana
 def calcular_mediana(lista):
     # TODO: ordenar a lista e encontrar o elemento do meio
     # 💡 Dica: se o tamanho for par, tire a média dos dois elementos centrais
-    pass
+    # Primeiro, ordenamos a lista
+    lista_ordenada = sorted(lista)
+    n = len(lista_ordenada)
+    meio = n // 2
+
+    # Se o tamanho for ímpar, retorna o elemento do meio
+    if n % 2 != 0:
+        return lista_ordenada[meio]
+    else:
+        # Se for par, retorna a média dos dois elementos centrais
+        return (lista_ordenada[meio - 1] + lista_ordenada[meio]) / 2
 
 
 # Função para calcular a moda
 def calcular_moda(lista):
     # TODO: encontrar o valor que mais aparece
     # 💡 Dica: use um dicionário para contar as ocorrências
-    pass
+    contagem = {}
+    for numero in lista:
+        if numero in contagem:
+            contagem[numero] += 1
+        else:
+            contagem[numero] = 1
+
+    # Encontra o maior valor de ocorrência
+    maior_frequencia = max(contagem.values())
+
+    # Retorna o(s) número(s) que aparecem mais vezes
+    # (Aqui retornamos apenas um valor, o primeiro mais frequente)
+    for numero, frequencia in contagem.items():
+        if frequencia == maior_frequencia:
+            return numero
 
 
 def main():
