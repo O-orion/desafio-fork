efa0153fbaaedba37456c9de41bf37fe27710146
Orion <orion@gmail.com>
2025-11-03 15:02:06 -0400

    teste 2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Instruções:
 
 # Função para calcular a média
 def calcular_media(lista):
-    # Desafio conclu[ido]wwwwew
+    # Desafio conclu[ido]wwwwewwwewwewe
     # TODO: implementar awwwewwe soma dos wewewweweelementos e dividir pelo tamanho da lista
     pass
 
